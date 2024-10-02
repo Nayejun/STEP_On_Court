51fd26557b6231f4330262c3eef97db944e0ba0c
Nayejun <ohman0705@naver.com>
2024-10-02 14:57:15 +0900

    Update Login Page

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -5,6 +5,8 @@ import { Input } from "@/components/ui/input";
 import { validateLogin } from "@/utils/validations";
 import { ILogin } from "@/types/types";
 import useStore from "@/utils/store";
+import Stack from "@app/components/ui/stack";
+import { Button } from "@app/components/ui/button";
 
 function Login() {
    const [loginData, setLoginData] = useState<ILogin>({
@@ -27,7 +29,7 @@ function Login() {
    };
 
    return (
-      <div>
+      <Stack justify="center" align="center" flexDirection="column">
          <form onSubmit={handleSubmit}>
             <Input
                type="email"
@@ -37,16 +39,18 @@ function Login() {
                placeholder="Email"
             />
 
-            <input
+            <Input
                type="password"
                name="password"
                value={loginData.password}
                onChange={handleChange}
                placeholder="Password"
             />
-            <button type="submit">Login</button>
+            <Stack justify="center" align="center">
+               <Button type="submit">Login</Button>
+            </Stack>
          </form>
-      </div>
+      </Stack>
    );
 }
 
