f5e0679c9828458aa00416f1555334a5d743aa2c
Nayejun <ohman0705@naver.com>
2024-10-02 09:41:08 +0900

    I Love Korea

--- client/app/utils/validations.ts
+++ client/app/utils/validations.ts
@@ -1,11 +1,11 @@
 import { z } from "zod";
 
 const signupSchema = z.object({
-   username: z.string().min(1, { message: "Username is required" }),
-   email: z.string().email({ message: "Invalid email address" }),
+   username: z.string().min(1, { message: "이름은 필수입니다." }),
+   email: z.string().email({ message: "이메일 주소를 확인해주세요." }),
    password: z
       .string()
-      .min(6, { message: "Password must be at least 6 characters long" }),
+      .min(6, { message: "비밀번호는 6자 이상이어야 합니다." }),
 });
 
 type SignupData = z.infer<typeof signupSchema>;
