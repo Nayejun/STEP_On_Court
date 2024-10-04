f9340ece2aa898cd9eca6d472d61b3828aba69c3
Nayejun <ohman0705@naver.com>
2024-10-04 15:04:08 +0900

    Add Calendar Events Logic

--- client/app/components/ui/calendar.tsx
+++ client/app/components/ui/calendar.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import { cn } from "@/app/lib/utils";
+import useStore from "@app/utils/store";
 
 export type CalendarProps = {
    className?: string;
@@ -20,6 +21,29 @@ function CustomCalendar({
    const [value, setValue] = useState(new Date());
    const [events, setEvents] = useState<{ [key: string]: string[] }>({});
 
+   // event dummy data
+   const dummyData = [
+      { date: "Thu Oct 17 2024", event: "Event 1" },
+      { date: "Thu Oct 18 2024", event: "Event 2" },
+      { date: "Thu Oct 19 2024", event: "Event 3" },
+   ];
+
+   // real event data
+   const eventData = useStore((state) => state.reservations);
+
+   useEffect(() => {
+      const newEvents: { [key: string]: string[] } = {};
+      dummyData.forEach((reservation) => {
+         const dateString = new Date(reservation.date).toDateString();
+         if (!newEvents[dateString]) {
+            newEvents[dateString] = [];
+         }
+         newEvents[dateString].push(reservation.event);
+      });
+      setEvents(newEvents);
+   }, []);
+
+   // @useGuard 'Admin'
    const handleDayClick = (date: Date) => {
       const dateString = date.toDateString();
       const event = prompt("일정 추가 :");
@@ -44,6 +68,11 @@ function CustomCalendar({
       );
    };
 
+   const tileClassName = ({ date, view }: { date: Date; view: string }) => {
+      const dateString = date.toDateString();
+      return events[dateString] ? "highlight" : "";
+   };
+
    return (
       <div className={cn("p-3", className)}>
          <div className="selected-date">테스트 {value.toDateString()}</div>
@@ -56,6 +85,7 @@ function CustomCalendar({
             value={value}
             onClickDay={handleDayClick}
             tileContent={tileContent}
+            tileClassName={tileClassName}
             {...props}
          />
       </div>
